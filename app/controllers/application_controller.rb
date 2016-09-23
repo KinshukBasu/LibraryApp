4bbcab6ab4ee61b6f938a707d78720c1022722b4
SmokingSadhus <amedhek@ncsu.edu>
2016-09-23 00:25:30 -0400

    Added Room creation and booking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,4 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   end
-end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912210008) do
+ActiveRecord::Schema.define(version: 20160919043425) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer  "userid"
@@ -22,9 +22,8 @@ ActiveRecord::Schema.define(version: 20160912210008) do
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.integer  "room_no"
     t.string   "location"
-    t.string   "size"
+    t.integer  "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
