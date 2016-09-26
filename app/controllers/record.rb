9fba3deef2ad7308a98414ad859a057571357246
SmokingSadhus <amedhek@ncsu.edu>
2016-09-25 23:30:34 -0400

    display change

--- app/controllers/record.rb
+++ app/controllers/record.rb
@@ -8,12 +8,12 @@ module Record
   def getPastBookings
     userID = session[:user_id]
     time = (Time.now)
-    @booking = Booking.where('userid = ? AND (outtime < ?)',userID,time)
+    @booking = Booking.where('userid = ? AND (intime < ?)',userID,time)
   end
 
   def getOngoingBookings
     userID = session[:user_id]
     time = (Time.now)
-    @booking = Booking.where('userid = ? AND (intime < ?) AND (outtime >= ?)',userID,time,time)
+    @booking = Booking.where('userid = ? AND (intime < ?)' ,userID,time)
   end
 end
